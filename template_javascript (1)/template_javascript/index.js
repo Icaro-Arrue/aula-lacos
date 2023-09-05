17a0675df3fcafb4da6f45a50e5b184b48b2ea4e
Livia-Arr <liviapereira0516@gmail.com>
2023-09-04 21:56:51 -0300

    socorro

---
+++ template_javascript (1)/template_javascript/index.js
@@ -0,0 +1,16 @@
+//1.  exibe o valor desde q seja menor q 5, adicionando 1 ao valor a cada vez q ele passa pelo loop
+//no console
+//2
+//4
+
+//2. exibe os números maiores que 18 na lista
+//sim
+/* for(let numero of listas){
+console.log(i)
+
+}
+
+*/
+
+
+//3. ****
